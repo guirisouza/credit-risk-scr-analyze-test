7a51f13cb36d4304c2543b9602c82524f94ec989
Guilherme <guilherme@pop-os.localdomain>
2020-06-05 23:44:02 -0300

    fix variables and render small bugs

--- api/resources/scr/report.py
+++ api/resources/scr/report.py
@@ -1,9 +1,5 @@
 import falcon
-import locale
-from decimal import Decimal
-from datetime import datetime
 import json
-import os
 import requests
 from utils.reporter_generator import reporter_generator
 
@@ -73,6 +69,7 @@ def sorted_due(due_operations, operation_code):
                 dct_due['due_description'] = due['due_type']['description']
                 dct_due['category_description'] = due['category_sub']['description']
                 dct_due['total_value'] += round(due['due_value'], 2)
+                dct_due['word_identifier'] = operation_code
         total_value_dues += round(dct_due['total_value'], 2)
         dct_due['total_value_dues'] = round(total_value_dues,2)
         risk[operation_code] = round(dct_due['total_value_dues'], 2)
@@ -90,25 +87,29 @@ def sorted_due(due_operations, operation_code):
 
 class Report:
     def on_get(self, req, res):
-        data_operations = []
-        data = src_data_request()
-        operations_items = data['operation_items']
-        infos = {
-            'reference_data': reverse_data(data['reference_date']),
-            'start_relationship': reverse_data(data['start_relationship']),
-            'operation_count': data['operation_count']
-        }
-        a_operations = sorted_due(operations_items, 'a')
-        b_operations = sorted_due(operations_items, 'h')
-        a_operations['data'][0]['total_value_dues'] = a_operations['risk']['a']
-        data_operations.append(a_operations['data'])
-        data_operations.append(b_operations['data'])
-        scr_stats = calculate_risk([a_operations['risk'], b_operations['risk']])
-        reporter_generator(data_operations, scr_stats, infos)
+        try:
+            data = src_data_request()
+            data_operations = []
+            operations_items = data['operation_items']
+            a_operations = sorted_due(operations_items, 'a')
+            b_operations = sorted_due(operations_items, 'h')
+            infos = {
+                'reference_data': reverse_data(data['reference_date']),
+                'start_relationship': reverse_data(data['start_relationship']),
+                'financial_institution_count': data['financial_institution_count'],
+            }
+            a_operations['data'][0]['total_value_dues'] = a_operations['risk']['a']
+            data_operations.append(a_operations['data'])
+            data_operations.append(b_operations['data'])
+            scr_stats = calculate_risk([a_operations['risk'], b_operations['risk']])
+            reporter_generator(data_operations, scr_stats, infos)
 
-        filename="./report.pdf"
-        res.downloadable_as = filename
-        res.content_type = 'application/pdf'
-
-        res.stream= open(filename, 'rb')
-        res.status = falcon.HTTP_200
+            filename="./report.pdf"
+            res.downloadable_as = filename
+            res.content_type = 'application/pdf'
+            res.stream= open(filename, 'rb')
+            res.status = falcon.HTTP_200
+        
+        except:
+            res.status = falcon.HTTP_400
+            res.body = json.dumps({"message":"Não foi possível gerar o relatório"})
--- api/resources/scr/scr.py
+++ api/resources/scr/scr.py
@@ -1,8 +1,5 @@
 import falcon
-from decimal import Decimal
-from datetime import datetime
 import json
-import os
 import requests
 
 def src_data_request():
@@ -12,18 +9,22 @@ def src_data_request():
 
 class Scr:
     def on_get(self, req, res):
-        data = src_data_request()
-        due_lst = []
-        for due in data['operation_items']:
-            due_blueprint = {}
-            due_blueprint['category_description'] = due['category_sub']['category']['category_description']
-            due_blueprint['category_sub_description'] = due['category_sub']['description']
-            due_blueprint['due_description'] = due['due_type']['description']
-            due_blueprint['due_type_group'] = due['due_type']['due_type_group'] 
-            due_blueprint['due_value'] = due['due_value']
-            due_blueprint['due_code'] = due['due_type']['due_code']
-            due_lst.append(due_blueprint)
-        data['operation_items'] = due_lst
+        try:
+            data = src_data_request()
+            due_lst = []
+            for due in data['operation_items']:
+                due_blueprint = {}
+                due_blueprint['category_description'] = due['category_sub']['category']['category_description']
+                due_blueprint['category_sub_description'] = due['category_sub']['description']
+                due_blueprint['due_description'] = due['due_type']['description']
+                due_blueprint['due_type_group'] = due['due_type']['due_type_group'] 
+                due_blueprint['due_value'] = due['due_value']
+                due_blueprint['due_code'] = due['due_type']['due_code']
+                due_lst.append(due_blueprint)
+            data['operation_items'] = due_lst
 
-        res.body = json.dumps(data)
+            res.body = json.dumps(data)
+        except:
+            res.status = falcon.HTTP_400
+            res.body = json.dumps({"message":"Não foi possível gerar o consolidado SCR"})
         
